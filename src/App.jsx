e57b9f4a2a4dcb6507eaea39cbe35056e77f7297
Felipe Lopes <felipemlopes0009@gmail.com>
2024-04-13 09:49:16 -0400

    Home page styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Grid from "./components/Grid";
-import Header from "./components/Header";
+//import Grid from "./components/Grid";
+//import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Header />
-      <Grid />
+      <Home />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
+import "../styles/general.css";
+import "../styles/home.css";
+
 const Home = () => {
   return (
     <>
-      <h2>Mangas & Books All in One Place</h2>
-      <h3>Search, rate, list and share your favorite mangas and books </h3>
-      <input type="text" placeholder="Search" />
+      <main>
+        <div>
+          <h2>Mangas & Books All in One Place</h2>
+          <h3>Search, rate, list and share your favorite mangas and books </h3>
+        </div>
+        <div className="searchbar-container">
+          <input type="text" placeholder="Search" className="searchbar" />
+        </div>
+      </main>
     </>
   );
 };
