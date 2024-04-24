336fbdafe3ea23c84251e5e94b6403bca078a278
lopesmfelipe <felipemlopes0009@gmail.com>
2024-04-24 17:04:58 -0400

    Home page search area

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,27 @@
 import "../styles/general.css";
 import "../styles/home.css";
-import logo from "./../assets/images/retrologo.jpg";
 
 const Home = () => {
   return (
-    <>
+    <section>
       <header>
-        <div className="logo-container">
-          <img src={logo} />
-          <h3>MangaBook</h3>
-        </div>
-
         <div className="menu">
-          <p>Readlist</p>
-          <p>Profile</p>
+          <button>Readlist</button>
+          <button>Profile</button>
         </div>
       </header>
-      <main className="background-image">
-        <div>
-          <h2>Mangas & Books All in One Place</h2>
+      <main>
+        <div className="text-container">
+          <h1 className="title">MANGABOOK</h1>
+          <h2>Mangas & Books All in One Place!</h2>
           <h3>Search, rate, list and share your favorite mangas and books </h3>
         </div>
         <div className="searchbar-container">
           <input type="text" placeholder="Search" className="searchbar" />
         </div>
       </main>
-    </>
+
+    </section>
   );
 };
 
